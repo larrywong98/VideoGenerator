34f6289fb256f9b960410f6ed07c2d30a4099070
Larry Wang <larrywong9898@gmail.com>
2022-04-03 21:40:35 -0700

    Demo Starter

---
+++ src/main/java/Video/VideoGenerator.java
@@ -0,0 +1,11 @@
+//package Video;
+//import org.opencv.core.Core;
+//
+//public class VideoGenerator {
+//    static {
+//        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+//    }
+//    public static void main(String[] args) {
+//        System.loadLibrary("ok");
+//    }
+//}
